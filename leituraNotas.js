0012c31b2e05859c04ba28fc0e23b5f149717d0c
Rachel <rachellizandra@outlook.com>
2023-02-08 21:21:43 -0300

    adiciona lógica para para converter o arquivo txt em array de objetos

---
+++ leituraNotas.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const Nota = require('./notas')
+
+const notas = fs.readdirSync(path.resolve('notas'))
+
+const notasParsed = notas.map((file) => {
+    const raw = fs.readFileSync(path.resolve('notas', file), "utf-8") //importar e leitura dos arquivos notas txt
+    const stringToArray = `[${raw.replaceAll("}", "},")}]`//substituição de caracteres
+    const normalizarData = stringToArray.replace(",]", "]").replaceAll(" ", "") //substituição de caracteres
+    const parsedData = eval(normalizarData); //converter texto em objeto javascript
+    /* pode dar o return */ parsedData.map((nota) => Object.assign(nota, {file})); //adiciona o arquivo em cada objeto
+    return parsedData.map((nota) => {
+        const qtdProdutoNumber = +nota["quantidade_produto"] //indica que a chave quantidade_produto é do tipo number
+        return new Nota(nota["id_pedido"], nota["número_item"], qtdProdutoNumber, file)
+    })
+}).flatMap((nota) => nota)
+//.forEach((nota) => console.log("A quantidade é: " + nota.calcularQtdProdutos())) //transforma o array bidimensional em unidimensional
+
+module.exports = notasParsed
+
+fs.writeFileSync(path.resolve('Notas.txt'), JSON.stringify(notasParsed))
