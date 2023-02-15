186660392ae4624ac221d0d8b348999d5310ad9a
Rachel <rachellizandra@outlook.com>
2023-02-15 15:49:36 -0300

    modifica classe Utils

--- classes/principais/Utils.js
+++ classes/principais/Utils.js
@@ -2,7 +2,7 @@ class Utils {
     static validarValorInteiro(valor, chave) {
         if (!Number.isInteger(valor)) {
             //this significa o valor atual e a chave é o que está sendo acessado
-            throw new TypeError(`A propriedade ${chave} deve ser um número inteiro`);
+            throw new TypeError(`A propriedade ${chave} com valor ${valor} deve ser um número inteiro`);
           }
     }
 
