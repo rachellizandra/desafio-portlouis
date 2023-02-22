bed0fe5ae481a7290d22fca37ccf068d044424b6
Rachel <rachellizandra@outlook.com>
2023-02-22 15:56:49 -0300

    alterando a classe exception de notas

--- classes/exceptions/NotasInconsistentes.js
+++ classes/exceptions/NotasInconsistentes.js
@@ -1,6 +1,6 @@
 class NotasInconsistente extends Error{
     constructor(nota = {}) {
-        super(`Nota id ${nota.idPedido} e Nota ${nota.numeroItem} não existem.`)
+        super(`Nota id ${nota.id}, id_pedido ${nota.idPedido} e número_item ${nota.numeroItem} não existem.`)
     }
 }
 
